c736480fe961711705964d1d577defbb19e287ce
Harm <harmblankers@icloud.com>
2016-03-23 22:10:28 +0100

    BACKUP

---
+++ home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
+
+def home(request):
+    return render(request, 'home/home.html')
+
+def onderzoek(request):
+    return render(request, 'home/onderzoek.html')
